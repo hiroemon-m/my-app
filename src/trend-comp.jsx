375697789d455a42be53fa505dfed955af479056
Matsumoto Hirotomo <matsumoto-hirotomo@matsumotonoMacBook-Air.local>
2024-11-18 21:34:26 +0900

    fix

--- src/trend-comp.jsx
+++ src/trend-comp.jsx
@@ -67,7 +67,7 @@ const PlotBarChartB = ({ update, visualType, topic, company, clickdata, onRender
         // JSONデータの整形
         const formattedData = Object.entries(companyData).map(([key, value]) => ({
           category: key,
-          value: parseFloat(value) || 0, // 値を数値に変換（ない場合は0）
+          value: key === "" ? 0 : parseFloat(value) * 100 , // 値を数値に変換（ない場合は0）
         }));
 
         // データを降順にソートして上位10件を取得
